0885a6365055180175280688135e3db6792d8e2a
mabbuprem <mabbupremkumar@gmail.com>
2022-03-16 14:01:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AlgorithmProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("algorithm problems");
+
+            BubbleSort bubbleSort = new BubbleSort();
+            bubbleSort.UserInput<string>();
+
+            WordList wordList = new WordList();
+            wordList.UserInput<string>();
+        }
+    }
+}
